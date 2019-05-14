a24eae31c5b356d612891f6157a0c1a3c0976fc9
Rémy LENGLET <lenglet.remy@atomsit.com>
2019-05-14 15:09:15 +0200

    Initial ATOMSit module

---
+++ Database/Seeders/TenantAnalyticsDatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Analytics\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TenantAnalyticsDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
