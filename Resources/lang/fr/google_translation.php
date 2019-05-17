c100b8b78c5c838b015f981455698231ba349351
Rémy LENGLET <lenglet.remy@atomsit.com>
2019-05-17 12:27:11 +0200

    Complete translation for GlobalWidget

---
+++ Resources/lang/fr/google_translation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'widgets' => [
+        'global_widget' => [
+            'views' => [
+                'title' => "Informations primordiales",
+                'bounce_rate' => "Taux de rebond",
+                'avg_time' => "Temps moyen d'une visite",
+                'minute' => 'minute(s)',
+                'number_printing' => "Nombre d'affichages",
+                'direct_users' => "Visiteurs depuis un moteur de recherche",
+                'visitors' => "Visiteurs"
+            ]
+        ]
+    ]
+];
